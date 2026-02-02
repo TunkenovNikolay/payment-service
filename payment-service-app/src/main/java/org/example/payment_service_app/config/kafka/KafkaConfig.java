c5ee6b37841ec3efa6596fecb3611adbc2f5978c
Nikosh <nik0sh@bk.ru>
2026-02-02 08:54:30 +0700

    homework-18 | add async payment with kafka

--- payment-service-app/src/main/java/org/example/payment_service_app/config/kafka/KafkaConfig.java
+++ payment-service-app/src/main/java/org/example/payment_service_app/config/kafka/KafkaConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.example.payment_service_app.async.XPaymentAdapterRequestMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -15,20 +16,20 @@ import java.util.Map;
 
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
     @Bean
     public ProducerFactory<String, XPaymentAdapterRequestMessage> producerFactory() {
-        final Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(config);
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
     public KafkaTemplate<String, XPaymentAdapterRequestMessage> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
-
 }
---
+++ xpayment-adapter-app/src/main/java/org/example/xpayment_adapter_app/async/kafka/KafkaXPaymentAdapterMessageProducer.java
@@ -0,0 +1,30 @@
+package org.example.xpayment_adapter_app.async.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.xpayment_adapter_app.async.AsyncSender;
+import org.example.xpayment_adapter_app.async.XPaymentAdapterResponseMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaXPaymentAdapterMessageProducer implements AsyncSender<XPaymentAdapterResponseMessage> {
+    private final KafkaTemplate<String, XPaymentAdapterResponseMessage> template;
+    private final String topic;
+
+    public KafkaXPaymentAdapterMessageProducer(
+            KafkaTemplate<String, XPaymentAdapterResponseMessage> template,
+            @Value("${app.kafka.topics.xpayment-adapter.response:xpayment-adapter.responses}") String topic) {
+        this.template = template;
+        this.topic = topic;
+    }
+
+    @Override
+    public void send(XPaymentAdapterResponseMessage msg) {
+        String key = msg.getPaymentGuid().toString(); // фиксируем партиционирование по платежу
+        log.info("Sending XPayment Adapter response: guid={}, amount={}, currency = {} ->topic = {} ",
+            msg.getPaymentGuid(), msg.getAmount(), msg.getCurrency(), topic);
+        template.send(topic, key, msg);
+    }
+}
